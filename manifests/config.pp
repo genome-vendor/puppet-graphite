3246b522fae0e5e7302fdfea4457061b880aceba
Brian Derickson <bdericks@genome.wustl.edu>
2014-09-04 14:08:43 -0500

    allow user to specify whitelist and blacklist
    Added templates for whitelist.conf and blacklist.conf. Added params to
    graphite module allowing a list of regexes to be specified that are
    listed in repsective config files.

--- manifests/config.pp
+++ manifests/config.pp
@@ -187,7 +187,12 @@ class graphite::config inherits graphite::params {
     '/opt/graphite/conf/storage-aggregation.conf':
       mode    => '0644',
       content => template('graphite/opt/graphite/conf/storage-aggregation.conf.erb'),
-      #notify  => $notify_services;
+    '/opt/graphite/conf/whitelist.conf':
+      mode    => '0644',
+      content => template('graphite/opt/graphite/conf/whitelist.conf.erb'),
+    '/opt/graphite/conf/blacklist.conf':
+      mode    => '0644',
+      content => template('graphite/opt/graphite/conf/blacklist.conf.erb'),
   }
 
 
